803a50c525d36233a1d349b7e0b1638353b616bc
microcephalus7 <microcephalus7@gmail.com>
2021-10-12 14:09:52 +0900

    repeat 2021-10-12

--- repeat/2021-10-12/13.c
+++ repeat/2021-10-12/13.c
@@ -1,2 +1,76 @@
 // 몇 번 입력할 지 입력받은 후 사용자 값이 0보다 큰 정수값을 받아서 평균 값 출력
 // while, do-while, for 세가지 다 출력
+
+#include <stdio.h>
+
+void ScanInput (int *, int *);
+void PrintResult (int, int);
+
+int main (void) {
+    int len;
+    int sum = 0;
+
+    ScanInput(&len, &sum);
+    PrintResult(len, sum);
+
+    return 0;
+}
+
+void ScanInput (int * len, int * sum) {
+
+    
+
+    fputs("몇 번 입력? : ", stdout);
+    scanf("%d", len);
+
+    // while
+    // int i = 0;
+    // while (i < *len) {
+    //     int input = 0;
+
+    //     while (input <= 0) {
+    //         fputs("숫자 입력 : ", stdout);
+    //         scanf("%d", &input);
+    //     }
+    //     *sum += input;
+    //     i++;
+    // }
+
+    // for
+    // int i;
+
+    // for (i = 0; i < len; i++) {
+    //     int input = 0;
+
+    //     while (input <= 0) {
+    //         fputs("숫자 입력 : ", stdout);
+    //         scanf("%d", &input);
+    //     }
+    //     *sum += input;
+    // }
+
+    // do while
+    
+    int i;
+
+    for (i = 0; i < *len; i++) {
+        int input;
+
+        do {
+            fputs("숫자 입력 : ", stdout);
+            scanf("%d", &input);
+        } while (input <= 0);
+
+        *sum += input;
+    }
+    
+
+    return;
+
+}
+void PrintResult (int len, int sum) {
+    
+    printf("평균 : %lf \n", (double)sum / len);
+
+    return;
+}
--- repeat/2021-10-12/7.c
+++ repeat/2021-10-12/7.c
@@ -1,2 +1,72 @@
 // 10진수를 입력받아 2진수로 출력
 // 비트 연산자 쓴 것과 안 쓴 것 둘다 구현
+
+#include <stdio.h>
+
+void ScanInput (int *);
+void PrintResult (int);
+
+int main (void) {
+    int num;
+
+    // ScanInput(&num);
+    PrintResult(num);
+
+    return 0;
+}
+
+void ScanInput (int * num) {
+    
+    fputs("10진수 입력 : ", stdout);
+    scanf("%d", num);
+
+    return;
+
+}
+// 비트 연산자
+// void PrintResult (int num) {
+
+//     int i;
+
+//     fputs("2진수 : ", stdout);
+
+//     for (i = 7; i >= 0; i--) {
+//         if (num >> i & 1) {
+//             fputs("1", stdout);
+//         } else {
+//             fputs("0", stdout);
+//         }
+//     }
+
+//     fputs("\n", stdout);
+
+//     return;
+
+// }
+
+// 비트 연산자 x
+void PrintResult (int num) {
+    int i = 1;
+    int num1 = 12;
+
+    while (num1 > i) {
+        i *= 2;
+
+    }
+
+
+    fputs("2진수 : ", stdout);
+
+    while (i != 0) {
+        if (num1 >= i ) {
+            printf("1");
+            num1 -= i;
+        } else {
+            printf("0");
+        }
+        i /= 2;
+    }
+
+    return;
+
+}
